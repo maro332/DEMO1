4310476bb77963ed7e8ae852483d2cec01d89ab4
user <user@172.16.125.193>
2018-12-04 16:35:17 -0500

    add test boject+ tc2

---
+++ Object Repository/HOME_PAGE/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>fb0559a0-1a7f-4062-ae71-428c074a6ed9</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
