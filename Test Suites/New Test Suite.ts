3a6e84dc3fa76bc70f4bafbec96d9b66cabd044d
maro332 <lama.marouane@gmail.com>
2018-12-05 12:33:25 -0500

    add test suite, New test collection et data file

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>cfbd3076-34bd-43c9-a65d-d91cf5919d20</testSuiteGuid>
+</TestSuiteEntity>
